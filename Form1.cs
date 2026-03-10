04717e65c3b1e8683e181c401fba821e33ce7747
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-03-10 08:54:54 +0800

    Menambahkan implementasi konversi suhu

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,96 @@ namespace jurnal_mod3
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double nilaiPertama = 0;
+            double nilaiKedua = 0;
+            if (comboBox1.Text == "" || comboBox2.Text == "")
+            {
+                MessageBox.Show("Pilih Satuan terlebih dahulu!");
+            }
+            else if (textBox1.Text == "")
+            {
+                MessageBox.Show("Masukkan angka yang valid!");
+            }
+
+            if (comboBox1.Text == "Celcius" && comboBox2.Text == "Fahrenheit")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama * 9 / 5) + 32;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+            else if (comboBox1.Text == "Celcius" && comboBox2.Text == "Kelvin")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = nilaiPertama + 273.15;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+            else if (comboBox1.Text == "Celcius" && comboBox2.Text == "Reamur")
+            {
+                nilaiPertama += Convert.ToDouble(textBox1.Text);
+                nilaiKedua = nilaiPertama * 4 / 5;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+
+            if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Celcius")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama - 32) * 5 / 9;
+                textBox2.Text = nilaiKedua.ToString();
+
+            }
+            else if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Kelvin")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama - 32) * 5 / 9 + 273.15;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+            else if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Reamur")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama - 32) * 4 / 9;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+
+            if ((comboBox1.Text == "Kelvin" && comboBox2.Text == "Celcius"))
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = nilaiPertama - 273.15;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+            else if (comboBox1.Text == "Kelvin" && comboBox2.Text == "Fahrenheit")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama - 273.15) * 9 / 5 + 32;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+            else if (comboBox1.Text == "Kelvin" && comboBox2.Text == "Reamur")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama - 273.15) * 4 / 5;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+
+            if (comboBox1.Text == "Reamur" && comboBox2.Text == "Celcius")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = nilaiPertama * 5 / 4;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+            else if (comboBox1.Text == "Reamur" && comboBox2.Text == "Fahrenheit")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama * 9 / 4) + 32;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+            else if (comboBox1.Text == "Reamur" && comboBox2.Text == "Kelvin")
+            {
+                nilaiPertama = Convert.ToDouble(textBox1.Text);
+                nilaiKedua = (nilaiPertama * 5 / 4) + 273.15;
+                textBox2.Text = nilaiKedua.ToString();
+            }
+        }
     }
 }
